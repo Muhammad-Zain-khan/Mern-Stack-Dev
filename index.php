90a67d8076894a5d3497d015eba1f9b08a3a417c
Muhammad-Zain-khan <148897274+Muhammad-Zain-khan@users.noreply.github.com>
2023-10-26 13:17:10 -0700

    kdm

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="kdm.css">
+    <title>kdmagency</title>
+</head>
+
+<body>
+                                                                 <!-- header section -->
+
+    <header>
+        <main>
+            <nav>
+                <div class="logo">
+                    <div class="img-head">
+                        <img src="./images/headerlogo.png" alt="">
+                    </div>
+                </div>
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li class="m-ser"><a href="">Services
+                                <i class="fa fa-check"></i>
+                            </a>
+
+                            <ul class="sr-drp">
+                                <li><a href="">Search Engin optimization</a></li>
+                                <li><a href="">Paid Marketing</a></li>
+                                <li><a href="">Social Media Optimazation</a></li>
+                                <li><a href="">E-Commerce Development</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Contact Us</a></li>
+                        <li><a href="">Blogs</a></li>
+                        <div class="btn-m">
+                            <a href="tel:+923330666167"><i class="fa fa-phone"></i>+923330666167</a>
+                        </div>
+                    </ul>
+                </div>
+                <div class="tel-p">
+                    <i class="fa fa-bars"></i>
+                    <a href="tel:+923330666167"><i class="fa fa-phone"></i>+923330666167</a>
+                </div>
+            </nav>
+        </main>
+    </header>
+
+                                                             <!-- header section end-->
+
+   <div class="hiro-section">
+    <img src="./images/slidernew-v2.jpg" alt="">
+   </div>                                                          
+
+   <style>
+    .hiro-section
+    {
+        width:100%;
+        
+    }
+    .hiro-section img
+    {
+        width:100%;
+        padding: 50px 0px 0px 0px;        
+      
+    }
+   </style>
+</body>
+
+
+
+</html>
